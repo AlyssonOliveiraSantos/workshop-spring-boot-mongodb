7d4cc09c4d0623343f24e13d9715b7413d3ce60c
Alysson <alyssonn.bra@live.com>
2022-07-25 14:06:36 -0300

    Atualização de usuário com PUT

--- src/main/java/com/educandoweb/mongo/config/Instantiation.java
+++ src/main/java/com/educandoweb/mongo/config/Instantiation.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
 
 import com.educandoweb.mongo.domain.User;
 import com.educandoweb.mongo.repository.UserRepository;
 
+@Configuration
 public class Instantiation implements CommandLineRunner{
 	
 	@Autowired
